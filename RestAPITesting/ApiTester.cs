9ca8600159dd335be85d4a1b396e49042b5bd714
suhas-achar <suhas.achar@gmail.com>
2021-06-16 23:46:55 +0530

    REST-API-Test

--- RestAPITesting/ApiTester.cs
+++ RestAPITesting/ApiTester.cs
@@ -5,28 +5,77 @@ using REST.API;
 using RestAPITesting.Utility;
 using Selenium.DotNet.App;
 using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace RestAPITesting
 {
+    /// <summary>
+    /// https://www.automatetheplanet.com/mstest-cheat-sheet/
+    /// </summary>
     [TestClass]
     public class ApiTester
     {
-        private string _uri;
-        private IAssertWrapper _assertWrapper;
-        private IApiConnection _apiConnection;
-        private ApiEndPoints _apiEndPoints;
+        private static string _uri;
+        private static IAssertWrapper _assertWrapper;
+        private static IApiConnection _apiConnection;
+        private static ApiEndPoints _apiEndPoints;
 
-        [TestInitialize]
-        public void CreateHttpClient()
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
         {
+            /***** Executes once before the test run. (Optional)  *****/
+            Debug.WriteLine("***** 1. [AssemblyInitialize] *****");
+
+        }
+
+        /// <summary>
+        /// For [ClassInitialize] to work, the method shouble static, public and should take one parametert "TestContext".
+        /// </summary>
+        /// <param name="context"></param>
+        [ClassInitialize]
+        public static void CreateHttpClient(TestContext context)
+        {
+            /***** Executes once for the test class. (Optional) *****/
+            Debug.WriteLine("***** 2. [ClassInitialize] *****");
+
             _uri = "https://jsonplaceholder.typicode.com";
             _assertWrapper = new MSTestAssertWrapper();
             _apiConnection = new HttpClientApiConnection();
             _apiEndPoints = new ApiEndPoints(_apiConnection);
         }
 
+        [TestInitialize]
+        public void Setup()
+        {
+            /*****  Runs before each test. (Optional) *****/
+            Debug.WriteLine("***** 3. [TestInitialize] *****");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            /***** Runs after each test. (Optional) *****/
+            Debug.WriteLine("***** 4. [TestCleanup] *****");
+        }
+
+        [ClassCleanup]
+        public static void TestFixtureTearDown()
+        {
+            /***** Runs once after all tests in this class are executed. (Optional) *****/
+            /***** Not guaranteed that it executes instantly after all tests from the class.  *****/
+            Debug.WriteLine("***** 5. [ClassCleanup] *****");
+        }
+
+        
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            /*****  Executes once after the test run. (Optional) *****/
+            Debug.WriteLine("***** 6. [AssemblyCleanup] *****");
+        }
 
         /// <summary>
         /// This particaular test case (Asynchronously_Runnig_Test) runs Asynchronously. 
@@ -46,7 +95,7 @@ namespace RestAPITesting
         [TestMethod]
         public void GET_RestApi_Test_1()
         {
-            Task<User> user = _apiEndPoints.GET_Using_GetAsync(_uri+"/users/1");
+            Task<User> user = _apiEndPoints.GET_Using_GetAsync(_uri + "/users/1");
 
             _assertWrapper.AreEqual("Sincere@april.biz", user.Result.email);
         }
